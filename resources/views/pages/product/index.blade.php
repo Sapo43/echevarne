1bb984be70f1cabbbd9ea0e225410610ce29671f
Matias Blanco <smbpy43@gmail.com>
2021-06-18 11:27:30 -0300

    fix product single

--- resources/views/pages/product/index.blade.php
+++ resources/views/pages/product/index.blade.php
@@ -6,7 +6,6 @@
     @include('includes.pageheaderarea')
     <!--== End Page Header Area ==-->
 
-    <!--== Start Page Content Wrapper ==-->
     <div class="page-content-wrapper sp-y">
         <div class="product-details-page-content">
             <div class="container container-wide">
@@ -279,49 +278,8 @@
             </div>
         </div>
     </div>
-    <!--== End Page Content Wrapper ==-->
-
-   @endsection
-
-
-<script>
-   function selectByName(slug) { 
- 
- $('#cantidadCarrito').removeClass('aumentoCarrito')
- $.ajax({
-   url: '/cart/add/'+slug,
- type : 'GET',
-   success: function(respuesta) {
-console.log(respuesta)
-   if(respuesta.cantidad==1){
-     iziToast.show({
-   title: 'Ok',
-   color: 'green',
-   message: respuesta.msg,
-   timeout: 2000,
-});
-$('#cantidadCarrito').html(parseInt($('#cantidadCarrito').html())+respuesta.cantidad);
-$('#cantidadCarrito').addClass('aumentoCarrito')
-   }else {
-     iziToast.show({
-   title: 'Error',
-   color: 'red',
-   message: respuesta.msg,
-   timeout: 2000,
-});
-   }
-
-
- 
-   },
-   error: function() {
-       console.log("No se ha podido obtener la información");
-   }
-});
- 
-}
 
 
 
+@endsection
 
-</script>
